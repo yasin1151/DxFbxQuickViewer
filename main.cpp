61a672935f4f5892502fea88c097e5a27c5e7eb3
yasin1151 <yasin1151@outlook.com>
2021-12-12 16:14:46 +0800

    directx 11 basic framework

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #define WIN32_LEAN_AND_MEAN 1
 
-#include <Windows.h>
+#include <d3d11.h>
+#include <d3dx11.h>
 #include <iostream>
+#include <Windows.h>
 #include <log/easylogging++.h>
+#include <wrl/client.h>
+
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dx11d.lib")
+
 
 using namespace std;
+using namespace Microsoft::WRL;
 
 // 标题名字，宽高
 const wchar_t* TITLE_NAME = L"FBXQuickViewer";
@@ -19,8 +27,77 @@ struct DeviceData
 {
     HWND hWnd;
     HINSTANCE hInstnace;
+
+	// dx 相关定义
+    ComPtr<ID3D11Device> pDevice;
+    ComPtr<ID3D11DeviceContext> pDeviceContext;
+    ComPtr<IDXGISwapChain> pSwapChain;
+    ComPtr<ID3D11RenderTargetView> pRenderTargetView;
+    ComPtr<ID3D11Texture2D> pDepthStencilTexture;
+    ComPtr<ID3D11DepthStencilView> pDepthStencilView;
+	
 };
 DeviceData g_DeviceData;
+
+HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow);
+HRESULT InitDevice(HWND hWnd, HINSTANCE hInstance);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+void Render();
+
+void OpenConsole()
+{
+    AllocConsole();
+    FILE* pCout;
+    freopen_s(&pCout, "CONOUT$", "w", stdout);
+    freopen_s(&pCout, "CONOUT$", "w", stderr);
+    freopen_s(&pCout, "CONIN$", "r", stdin);
+    if (pCout)
+        fclose(pCout);
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+    ZeroMemory(&g_DeviceData, sizeof(g_DeviceData));
+
+    // debug模式下打开控制台
+#ifdef _DEBUG
+    OpenConsole();
+#endif
+
+	
+    if (FAILED(InitWindow(hInstance, nCmdShow)))
+    {
+        LOG(ERROR) << "InitWindow Failed :" << GetLastError();
+        return 0;
+    }
+
+    if (FAILED(InitDevice(g_DeviceData.hWnd, hInstance)))
+    {
+        LOG(ERROR) << "InitDevice Failed :" << GetLastError();
+        return 0;
+    }
+
+    LOG(INFO) << "Init Success!!! \n" ;
+	
+    MSG msg = { nullptr };
+    while (WM_QUIT != msg.message)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+            Render();
+        }
+    }
+
+    return static_cast<int>(msg.wParam);
+}
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -34,7 +111,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     }
     return 0;
 }
-
 
 HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow)
 {
@@ -69,60 +145,110 @@ HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow)
     return S_OK;
 }
 
-void OpenConsole()
+HRESULT InitDevice(HWND hWnd, HINSTANCE hInstance)
 {
-    AllocConsole();
-    FILE* pCout;
-    freopen_s(&pCout, "CONOUT$", "w", stdout);
-    freopen_s(&pCout, "CONOUT$", "w", stderr);
-    freopen_s(&pCout, "CONIN$", "r", stdin);
-    if (pCout)
-        fclose(pCout);
+    HRESULT hr = S_OK;
+
+    RECT rc = {};
+    GetClientRect(hWnd, &rc);
+    UINT width = rc.right - rc.left;
+    UINT height = rc.bottom - rc.top;
+
+    UINT createDeviceFlags = 0;
+#ifdef _DEBUG
+    createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+    D3D_FEATURE_LEVEL featureLevels[] =
+    {
+        D3D_FEATURE_LEVEL_11_0,
+        D3D_FEATURE_LEVEL_10_1,
+        D3D_FEATURE_LEVEL_10_0,
+    };
+    UINT numFeatureLevels = ARRAYSIZE(featureLevels);
+
+    DXGI_SWAP_CHAIN_DESC sd;
+    ZeroMemory(&sd, sizeof(sd));
+    sd.BufferCount = 1;
+    sd.BufferDesc.Width = width;
+    sd.BufferDesc.Height = height;
+    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    sd.BufferDesc.RefreshRate.Numerator = 60;
+    sd.BufferDesc.RefreshRate.Denominator = 1;
+    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    sd.OutputWindow = hWnd;
+    sd.SampleDesc.Count = 1;
+    sd.SampleDesc.Quality = 0;
+    sd.Windowed = TRUE;
+
+
+    hr = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, createDeviceFlags, featureLevels, numFeatureLevels,
+                                       D3D11_SDK_VERSION, &sd, &g_DeviceData.pSwapChain, &g_DeviceData.pDevice, nullptr, &g_DeviceData.pDeviceContext);
+    if (FAILED(hr))
+        return hr;
+
+    ComPtr<ID3D11Texture2D> pBackBuffer;
+    hr = g_DeviceData.pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), &pBackBuffer);
+
+    if (FAILED(hr))
+        return hr;
+
+
+    hr = g_DeviceData.pDevice->CreateRenderTargetView(pBackBuffer.Get(), nullptr, &g_DeviceData.pRenderTargetView);
+    if (FAILED(hr))
+        return hr;
+
+
+    D3D11_TEXTURE2D_DESC descDepth;
+    ZeroMemory(&descDepth, sizeof(descDepth));
+    descDepth.Width = width;
+    descDepth.Height = height;
+    descDepth.MipLevels = 1;
+    descDepth.ArraySize = 1;
+    descDepth.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+    descDepth.SampleDesc.Count = 1;
+    descDepth.SampleDesc.Quality = 0;
+    descDepth.Usage = D3D11_USAGE_DEFAULT;
+    descDepth.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+    descDepth.CPUAccessFlags = 0;
+    descDepth.MiscFlags = 0;
+    hr = g_DeviceData.pDevice->CreateTexture2D(&descDepth, NULL, &g_DeviceData.pDepthStencilTexture);
+    if (FAILED(hr))
+        return hr;
+
+    // Create the depth stencil view
+    D3D11_DEPTH_STENCIL_VIEW_DESC descDSV;
+    ZeroMemory(&descDSV, sizeof(descDSV));
+    descDSV.Format = descDepth.Format;
+    descDSV.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+    descDSV.Texture2D.MipSlice = 0;
+    hr = g_DeviceData.pDevice->CreateDepthStencilView(g_DeviceData.pDepthStencilTexture.Get(), &descDSV, &g_DeviceData.pDepthStencilView);
+    if (FAILED(hr))
+        return hr;
+
+    g_DeviceData.pDeviceContext->OMSetRenderTargets(1, g_DeviceData.pRenderTargetView.GetAddressOf(), g_DeviceData.pDepthStencilView.Get());
+
+
+    D3D11_VIEWPORT vp;
+    vp.Width = static_cast<FLOAT>(width);
+    vp.Height = static_cast<FLOAT>(height);
+    vp.MinDepth = 0.0f;
+    vp.MaxDepth = 1.0f;
+    vp.TopLeftX = 0;
+    vp.TopLeftY = 0;
+    g_DeviceData.pDeviceContext->RSSetViewports(1, &vp);
+	
 }
 
 
 void Render()
 {
+    float ClearColor[4] = { 0.0f, 0.125f, 0.3f, 1.0f };
+	
+    g_DeviceData.pDeviceContext->ClearRenderTargetView(g_DeviceData.pRenderTargetView.Get(), ClearColor);
+    g_DeviceData.pDeviceContext->ClearDepthStencilView(g_DeviceData.pDepthStencilView.Get(), D3D11_CLEAR_DEPTH, 1.0f, 0);
+
+
+    g_DeviceData.pSwapChain->Present(0, 0);
 
 }
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
-{
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
-
-    ZeroMemory(&g_DeviceData, sizeof(g_DeviceData));
-
-    // debug模式下打开控制台
-#ifdef _DEBUG
-    OpenConsole();
-#endif
-
-	
-    if (FAILED(InitWindow(hInstance, nCmdShow)))
-    {
-        LOG(ERROR) << "InitWindow Failed :" << GetLastError();
-        return 0;
-    }
-
-    LOG(INFO) << "Init Success!!! \n" ;
-	
-    MSG msg = { nullptr };
-    while (WM_QUIT != msg.message)
-    {
-        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-        else
-        {
-            Render();
-        }
-    }
-
-    return static_cast<int>(msg.wParam);
-}
-
-
-
